73c2fb2e31a873c7bc7909eb46a884d0b888a113
Zayra Penya Juan <zayrapenyajuan@gmail.com>
2026-02-23 11:04:43 +0000

    Practise 2 part 2 test create CalculatorTest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,14 @@ import org.example.Operation;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.calculate(Operation.ADD,2,6));
-        System.out.println(calculator.calculate(Operation.SUBSTRACT, 5,2));
-        System.out.println(calculator.calculate(Operation.MULTIPLY, 5,2));
-        System.out.println(calculator.calculate(Operation.DIVIDE, 9,2));
-
+        System.out.println(calculator.calculate(Operation.ADD, 2, 6));
+        System.out.println(calculator.calculate(Operation.SUBSTRACT, 5, 2));
+        System.out.println(calculator.calculate(Operation.MULTIPLY, 5, 2));
+        System.out.println(calculator.calculate(Operation.DIVIDE, 9, 2));
+        try {
+            calculator.calculate(Operation.DIVIDE, 5, 0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.calculate(Operation.ADD, 2, 3));
+    }
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calculator.calculate(Operation.SUBSTRACT, 10, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(35, calculator.calculate(Operation.MULTIPLY, 7, 5));
+    }
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.calculate(Operation.DIVIDE, 10, 5));
+    }
+    @Test
+    public void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculate(Operation.DIVIDE, 5, 0));
+    }
+
+    @Test
+    public void testAddByDecimal() {
+        assertEquals(5, calculator.calculate(Operation.ADD, 2.5, 2.5));
+    }
+    @Test
+    public void testMultiplyByDecimal() {
+        assertEquals(11.25, calculator.calculate(Operation.MULTIPLY, 2.5, 4.5));
+    }
+    @Test
+    public void testSubstractByNegative() {
+        assertEquals(-10, calculator.calculate(Operation.SUBSTRACT, -5, 5));
+    }
+
+}
